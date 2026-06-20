2f76bb06e8be655ab128c384855d4e08a99a9e7e
rishith2209 <lakshmirishithreddy_mannem@srmap.edu.in>
2026-06-20 10:55:34 +0530

    BOOKPROJECT folder

---
+++ BOOKPROJECT/BOOK_project/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/libraryDB").then(() => {
+    console.log("MongoDB Connection");
+}).catch((err) => {
+    console.log("No Connection");
+});
